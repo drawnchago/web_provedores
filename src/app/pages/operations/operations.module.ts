9c99ad8d577991043d9f54bd22188b18c4f2625f
Chehaniball <jose.bautista@phs.mx>
2021-09-01 16:24:17 -0500

    Mat card agregado

--- src/app/pages/operations/operations.module.ts
+++ src/app/pages/operations/operations.module.ts
@@ -3,12 +3,29 @@ import { CommonModule } from '@angular/common';
 
 import { OperationsRoutingModule } from './operations-routing.module';
 
+import { DemoMaterialModule } from '../../demo-material-module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { MatListModule } from '@angular/material/list';
+import { WorkOrdersComponent } from './process/work-orders/work-orders.component';
+import { WorkDialogComponent } from './process/work-orders/work-dialog/work-dialog.component';
+
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    WorkOrdersComponent,
+    WorkDialogComponent,
+  ],
   imports: [
     CommonModule,
-    OperationsRoutingModule
+    OperationsRoutingModule,
+    DemoMaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    MatListModule,
+    ReactiveFormsModule,
+    NgxDatatableModule
   ]
 })
 export class OperationsModule { }
